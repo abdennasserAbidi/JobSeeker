4ef621df453d8454ffab610e7719d7ff172bae16
Abdennasser <abdennasser.abidi@esprit.tn>
2025-12-03 13:06:03 +0100

    add config for uploading pdf

--- src/main/java/com/myjob/jobseeker/JobseekerApplication.java
+++ src/main/java/com/myjob/jobseeker/JobseekerApplication.java
@@ -31,6 +31,7 @@ public class JobseekerApplication {
 		Dotenv dotenv = Dotenv.configure()
 				.ignoreIfMissing()
 				.load();
+
 		// Set environment variables for Spring
 		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
 		System.setProperty("SOCKET_HOST", dotenv.get("SOCKET_HOST"));
@@ -44,9 +45,9 @@ public class JobseekerApplication {
 		System.setProperty("FILE_UPLOAD_DIR", dotenv.get("FILE_UPLOAD_DIR"));
 		System.setProperty("MAX_FILE_SIZE", dotenv.get("MAX_FILE_SIZE"));
 		System.setProperty("MAX_REQUEST_SIZE", dotenv.get("MAX_REQUEST_SIZE"));
-		System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
+		/*System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
 		System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
-		System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
+		System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));*/
 
 
 		SpringApplication.run(JobseekerApplication.class, args);
